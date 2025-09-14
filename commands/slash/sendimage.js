8c7d719ba3279495cd093285718ceb3142b5cf6f
Luffy <107133957+OrewaLuffy54@users.noreply.github.com>
2025-09-14 08:23:48 +0530

    Update sendimage.js

--- commands/slash/sendimage.js
+++ commands/slash/sendimage.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const path = require('path');
 
 // Replace these with the Discord User IDs of all owners
-const OWNER_IDS = ['123456789012345678', '987654321098765432']; // Add your additional owner IDs here
+const OWNER_IDS = ['123456789012345678', '1013832671014699130']; // Add your additional owner IDs here
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -32,3 +32,4 @@ module.exports = {
         }
     }
 };
+
