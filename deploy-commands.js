3fbb06405b26918405cc74a4ea47c451ad312f59
Luffy <107133957+OrewaLuffy54@users.noreply.github.com>
2025-09-14 17:05:27 +0530

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,43 @@ const { clientId, guildId, token } = require('./config.json');  // Make sure you
 
 const commands = [
     new SlashCommandBuilder().setName('ping').setDescription('Check the bot\'s latency and uptime'),
+    new SlashCommandBuilder()
+        .setName('sudo')
+        .setDescription('Admin Command')
+        .addStringOption(option =>
+            option.setName('action')
+                .setDescription('Select an action to perform')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'msg', value: 'msg' },
+                    { name: 'react', value: 'react' },
+                    { name: 'dm', value: 'dm' },
+                    { name: 'reply', value: 'reply' },
+                    { name: 'edit', value: 'edit' },
+                    { name: 'delete', value: 'delete' },
+                    { name: 'pin', value: 'pin' },
+                    { name: 'unpin', value: 'unpin' },
+                    { name: 'nickname', value: 'nickname' },
+                    { name: 'timeout', value: 'timeout' },
+                    { name: 'kick', value: 'kick' },
+                    { name: 'ban', value: 'ban' },
+                    { name: 'purge', value: 'purge' },
+                    { name: 'announce', value: 'announce' },
+                    { name: 'role', value: 'role' },
+                    { name: 'voicekick', value: 'voicekick' },
+                    { name: 'move', value: 'move' }
+                )
+        )
+        .addStringOption(option =>
+            option.setName('target')
+                .setDescription('Target ID (Channel ID, Message ID, User ID)')
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName('content')
+                .setDescription('Content (Message text, emoji, nickname, role ID, duration in minutes, move target channel ID, etc.)')
+                .setRequired(true)
+        ),
     // Add other commands here
 ]
     .map(command => command.toJSON());
